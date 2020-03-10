7fbe8da5e8182d1b83d85f7f41f17752f9ce0e5d
Felipe Sabino Domingos Da Silva <48417687858@info.local>
2020-03-10 15:06:40 -0300

    Inicio de projeto com BD criado

---
+++ BACK-END/Senai.Senatur.WebApi/Senai.Senatur.WebApi/Contexts/SenaturContext.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Senai.Senatur.WebApi.Domains
+{
+    public partial class SenaturContext : DbContext
+    {
+        public SenaturContext()
+        {
+        }
+
+        public SenaturContext(DbContextOptions<SenaturContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Pacotes> Pacotes { get; set; }
+        public virtual DbSet<TiposUsuario> TiposUsuario { get; set; }
+        public virtual DbSet<Usuarios> Usuarios { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Data Source=DEV101\\SQLEXPRESS; Initial Catalog=Senatur_Tarde; user Id=sa; pwd=sa@132;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Pacotes>(entity =>
+            {
+                entity.HasKey(e => e.IdPacote);
+
+                entity.HasIndex(e => e.NomePacote)
+                    .HasName("UQ__Pacotes__77BB3B4458FE1DC1")
+                    .IsUnique();
+
+                entity.Property(e => e.Ativo)
+                    .IsRequired()
+                    .HasDefaultValueSql("((1))");
+
+                entity.Property(e => e.DataIda).HasColumnType("date");
+
+                entity.Property(e => e.DataVolta).HasColumnType("date");
+
+                entity.Property(e => e.Descricao)
+                    .IsRequired()
+                    .HasMaxLength(1220)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.NomeCidade)
+                    .IsRequired()
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.NomePacote)
+                    .IsRequired()
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<TiposUsuario>(entity =>
+            {
+                entity.HasKey(e => e.IdTipoUsuario);
+
+                entity.Property(e => e.Titulo)
+                    .IsRequired()
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Usuarios>(entity =>
+            {
+                entity.HasKey(e => e.IdUsuario);
+
+                entity.HasIndex(e => e.Email)
+                    .HasName("UQ__Usuarios__A9D105342223E587")
+                    .IsUnique();
+
+                entity.Property(e => e.Email)
+                    .IsRequired()
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Senha)
+                    .IsRequired()
+                    .HasMaxLength(150)
+                    .IsUnicode(false);
+
+                entity.HasOne(d => d.IdTipoUsuarioNavigation)
+                    .WithMany(p => p.Usuarios)
+                    .HasForeignKey(d => d.IdTipoUsuario)
+                    .HasConstraintName("FK__Usuarios__IdTipo__5070F446");
+            });
+        }
+    }
+}
---
+++ BD/02_INSERCAO_BD.sql
@@ -0,0 +1,31 @@
+--INSERÇÃO DE DADOS DML
+
+USE Senatur_Tarde;
+
+INSERT INTO Pacotes (NomePacote, Descricao, DataIda, DataVolta, Valor, NomeCidade)
+VALUES ('SALVADOR - 5 DIAS / 4 DIÁRIAS','O que não falta em Salvador são atrações. Prova disso são as praias, os
+museus e as construções seculares que dão um charme mais que especial à região. A
+cidade, sinônimo de alegria, também é conhecida pela efervescência cultural que a
+credenciou como um dos destinos mais procurados por turistas brasileiros e
+estrangeiros. O Pelourinho e o Elevador são alguns dos principais pontos de visitação.','06/08/2020','10/08/2020',854.00,'Salvador'),
+	('RESORTS NA BAHIA - LITORAL
+NORTE - 5 DIAS / 4 DIÁRIAS','O Litoral Norte da Bahia conta com
+inúmeras praias emolduradas por coqueiros, além de piscinas naturais de águas mornas
+que são protegidas por recifes e habitadas por peixes coloridos. Banhos de mar em
+águas calmas ou agitadas, mergulho com snorkel, caminhada pela orla e calçadões,
+passeios de bicicleta, pontos turísticos históricos, interação com animais e até baladas
+estão entre as atrações da região. Destacam-se as praias de Guarajuba, Imbassaí,
+Praia do Forte e Costa do Sauipe.','14/05/2020','18/05/2020',1826.00,'Salvador'),
+	('BONITO VIA CAMPO GRANDE - 1
+PASSEIO - 5 DIAS / 4 DIÁRIAS','Localizado no estado de Mato Grosso do
+Sul e ao sul do Pantanal, Bonito possui centenas de cachoeiras, rios e lagos de águas
+cristalinas, além de cavernas inundadas, paredões rochosos e uma infinidade de peixes.
+Os aventureiros costumam render-se facilmente a esse destino regado por trilhas
+ecológicas, passeios de bote e descidas de rapel pelas inúmeras quedas d''água da
+região.','28/03/2020','01/04/2020',1004.00,'Bonito');
+
+INSERT INTO TiposUsuario (Titulo)
+VALUES ('Administrador'), ('Cliente');
+
+INSERT INTO Usuarios (Email, Senha, IdTipoUsuario)
+VALUES ('admin@admin.com','admin','1'), ('cliente@cliente.com','cliente','2');
