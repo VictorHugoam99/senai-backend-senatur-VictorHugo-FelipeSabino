7a59c87e0757c7f27552856ba547a5880ec4714c
Victor Hugo Alexandre Miranda <23871167800@info.local>
2020-03-11 15:51:51 -0300

    ParteVictor2

--- BACK-END/Senai.Senatur.WebApi/Senai.Senatur.WebApi/Controllers/UsuariosController.cs
+++ BACK-END/Senai.Senatur.WebApi/Senai.Senatur.WebApi/Controllers/UsuariosController.cs
@@ -23,7 +23,22 @@ namespace Senai.Senatur.WebApi.Controllers
             _usuariosRepository = new UsuariosRepository();
         }
 
+        /// <summary>
+        /// Lista todos os usuários
+        /// </summary>
+        /// <returns>Retorna uma lista de usuários e um status code 200 - Ok</returns>
+        /// dominio/api/Usuarios
+        /// <response code="200">Lista retornada com sucesso</response>
+        /// <response code="400">Erro em alguma parte da requisição</response>
+        /// <response code="401">Se o usuário não estiver logado</response>
+        /// <response code="403">Se o usuário não tiver permissão para tal ação</response>
+        [Authorize(Roles = "1")]    // Somente o tipo de usuário 1 (administrador) pode acessar o endpoint
         [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+
         public IActionResult Get()
         {
             return Ok(_usuariosRepository.Listar());
